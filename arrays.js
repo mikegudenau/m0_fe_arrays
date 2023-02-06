03a3af84cdd4f72cbeaa39fdc2458d95a53eb3ff
Mike Gudenau <mikegudenau@gmail.com>
2023-02-06 12:05:51 -0800

    add understanding of index positions

--- arrays.js
+++ arrays.js
@@ -16,3 +16,15 @@ ticket_prices.push(55, 47); // can push more than one elements
 // this method also changes the length of the array
 arrayOfBooleans.shift(); // removes the index[0] and returns false
 console.log(arrayOfBooleans); // updated array [ false, true, false ]
+
+// Understanding of index positions
+// counting starts at 0
+// friends variable: murphy is at index[0], tilden is at index[1], luRay is at index[2], Sam at index[3]
+console.log(friends[0]); // prints Murphy
+console.log(friends[1]); // prints Tilden
+console.log(friends[2]); // prints LuRay
+console.log(friends[3]); // prints Sam
+
+// we can also use the indexOf() method to find the index at which a given element can be found
+console.log(friends.indexOf('LuRay')); // prints 2
+console.log(friends.indexOf('Sam')); // prints 3
